59d0b796c0e5b0f947454d2404ec3986c8f88780
zedparman <ze.dparman@gmail.com>
2025-07-19 08:36:01 -0700

    Create App.tsx

---
+++ project/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <iframe src="../index.html" style={{ width: '100%', height: '100vh', border: 'none' }} />
+  );
+}
+
+export default App;
